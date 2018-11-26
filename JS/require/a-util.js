1882686909e94a87e75cfa3669c86fad03ce8417
agentleman <675740051@qq.com>
2018-11-26 22:24:17 +0800

    'require'

---
+++ JS/require/a-util.js
@@ -0,0 +1,8 @@
+define(['./util.js'],function(util){
+    var aUtil = {
+        aGetFormatDate:function(date){
+            return util.getFormateDate(date,2)
+        }
+    }
+    return aUtil
+})
---
+++ JS/webpack/src/app.js
@@ -0,0 +1,7 @@
+console.log(100);
+var $ = require('jquery');
+console.log($)
+var root = $('#root')
+root.html('<h6>zheshijquery</h6>')
+var aUtil = require('./a-util.js')
+aUtil.print()
